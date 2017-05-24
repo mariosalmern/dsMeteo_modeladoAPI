bbeb2158085188cf439b882bfb5f043dedb3a0eb
MarioSalmern <9waysofdestiny@gmail.com>
2017-05-24 12:58:10 +0200

    Commit desde android studio

--- app/src/main/java/com/example/monitor/comunicacion/MainActivity.java
+++ app/src/main/java/com/example/monitor/comunicacion/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvPagina;
     TextView textView_Mañana;
-
+    TextView textView_PasadoMañana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +25,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvPagina=(TextView)this.findViewById(R.id.tvPagina);
         textView_Mañana=(TextView)this.findViewById(R.id.textView_Mañana);
+        textView_PasadoMañana=(TextView)this.findViewById(R.id.textView_PasadoMañana);
     }
 
     public void cargar(View v){
         ComunicacionTask com=new ComunicacionTask();
         MañanaTask mañanaTask=new MañanaTask();
+        PasadoMañanaTask pasadoMañanaTask=new PasadoMañanaTask();
         com.execute("http://www.aemet.es/xml/municipios/localidad_09059.xml"); //Burgos
         mañanaTask.execute("http://www.aemet.es/xml/municipios/localidad_09059.xml"); //Burgos
+        pasadoMañanaTask.execute("http://www.aemet.es/xml/municipios/localidad_09059.xml");
     }
 
 
@@ -79,14 +82,14 @@ public class MainActivity extends AppCompatActivity {
 
                 //ASIGNAMOS LA MEDIA DE LAS PROB. DE PRECIPITACION AL TEXTVIEW.
                 cadena += "-Precipitaciones:" +
-                        media_prob_precipitacion + "% " +"\n";
+                        (long)(media_prob_precipitacion > 0 ? media_prob_precipitacion + 0.01 : media_prob_precipitacion - 0.01) + "% " +"\n";
 
                 //ASIGNAMOS LA TEMPERATURA MAXIMA Y MINIMA AL TEXTVIEW.
                 cadena += "-Temperaturas:" + "\n" +
                         "Máxima:  " +
-                        listaTempMax.item(0).getTextContent() + "\n"+
+                        listaTempMax.item(0).getTextContent() +"ºC" + "\n"+
                         "Mínima:  " +
-                        listaTempMin.item(0).getTextContent() + "\n" ;
+                        listaTempMin.item(0).getTextContent() +"ºC" + "\n" ;
 
 
                 //Asignamos el viento:
@@ -95,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                         "Dirección:  " +
                         listaDireccViento.item(0).getTextContent() + "\n"+
                         "Velocidad:  " +
-                        listaVientoVelocidad.item(0).getTextContent() + "\n" ;
+                        listaVientoVelocidad.item(0).getTextContent()+"km/h" + "\n" ;
 
 
             }
@@ -138,9 +141,12 @@ public class MainActivity extends AppCompatActivity {
                 NodeList listaTempMax = doc.getElementsByTagName("maxima");
                 NodeList listaTempMin = doc.getElementsByTagName("minima");
 
+                NodeList listaDireccViento = doc.getElementsByTagName("direccion");
+                NodeList listaVientoVelocidad = doc.getElementsByTagName("velocidad");
+
 
                 //MUESTRA DE LA LOCALIDAD SELECCIONADA:
-                cadena += listanombre.item(0).getTextContent() + "\n";
+                cadena += "·"+listanombre.item(0).getTextContent() + "\n";
 
                 //CALCULAMOS LA MEDIA DE LAS PROBABILIDADES DE PRECIPITACIÓN, ASEGURANDONOS ASÍ QUE NO COJEMOS VALORES NULOS DE LOS DISTINTOS PERIODOS DE PROBABILIDAD.
                 double media_prob_precipitacion_mañana = 0;
@@ -154,15 +160,20 @@ public class MainActivity extends AppCompatActivity {
                 media_prob_precipitacion_mañana = media_prob_precipitacion_mañana / 7;
 
                 //ASIGNAMOS LA MEDIA DE LAS PROB. DE PRECIPITACION AL TEXTVIEW.
-                cadena += "Prob. LLuvia:" + //Aproxima la media: (long)
-                        (long)(media_prob_precipitacion_mañana > 0 ? media_prob_precipitacion_mañana + 0.01 : media_prob_precipitacion_mañana - 0.01) + "% " +"\n";
+                cadena += "·Prob. LLuvia: " + (long)(media_prob_precipitacion_mañana > 0 ? media_prob_precipitacion_mañana + 0.01 : media_prob_precipitacion_mañana - 0.01) + "% " +"\n";
 
                 //ASIGNAMOS LA TEMPERATURA MAXIMA Y MINIMA AL TEXTVIEW.
-                cadena += "Temperatura:" + "\n" +
-                        "Máx:" +
-                        listaTempMax.item(3).getTextContent() + "\n"+
-                        "Mín:"   +
-                        listaTempMin.item(3).getTextContent() + "\n" ;
+                cadena += "·Temperatura:" + "\n" +
+                        "-Máx: " +
+                        listaTempMax.item(3).getTextContent() +"ºC" + "\n"+
+                        "-Mín: "   +
+                        listaTempMin.item(3).getTextContent() +"ºC" + "\n" ;
+
+                cadena += "·Vientos:" + "\n" +
+                        "-Dirección:  " +
+                        listaDireccViento.item(7).getTextContent() + "\n"+
+                        "-Velocidad:  " +
+                        listaVientoVelocidad.item(7).getTextContent()+" km/h" + "\n" ;
 
             }
             catch(Exception ex){
@@ -175,6 +186,82 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result){
 
             textView_Mañana.setText(result);
+
+        }
+
+    }
+
+    private class PasadoMañanaTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String cadena="";
+            try {
+                URL url = new URL(params[0]);
+                URLConnection con = url.openConnection();
+
+                InputStream is = con.getInputStream();
+                Document doc;
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder;
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(is);
+
+                //RECUPERAMOS LOS ELEMENTOS DEL API http://www.aemet.es/xml/municipios/localidad_09059.xml , EN ESTE CASO PARA BURGOS.
+                NodeList listanombre = doc.getElementsByTagName("nombre");
+                NodeList listaPrecip = doc.getElementsByTagName("prob_precipitacion");
+
+                NodeList listaTempMax = doc.getElementsByTagName("maxima");
+                NodeList listaTempMin = doc.getElementsByTagName("minima");
+
+                NodeList listaDireccViento = doc.getElementsByTagName("direccion");
+                NodeList listaVientoVelocidad = doc.getElementsByTagName("velocidad");
+
+
+                //MUESTRA DE LA LOCALIDAD SELECCIONADA:
+                cadena += "·"+listanombre.item(0).getTextContent() + "\n";
+
+                //CALCULAMOS LA MEDIA DE LAS PROBABILIDADES DE PRECIPITACIÓN, ASEGURANDONOS ASÍ QUE NO COJEMOS VALORES NULOS DE LOS DISTINTOS PERIODOS DE PROBABILIDAD.
+                double media_prob_precipitacion_pasado = 0;
+                for (int i = 14; i <= 16; i++) {
+                    String valor = listaPrecip.item(i).getTextContent();
+                    if (valor == null || valor.equals("")) {
+                        valor = "0";
+                    }
+                    media_prob_precipitacion_pasado += Double.parseDouble(valor);
+                }
+                media_prob_precipitacion_pasado = media_prob_precipitacion_pasado / 3;
+
+                //ASIGNAMOS LA MEDIA DE LAS PROB. DE PRECIPITACION AL TEXTVIEW.
+                cadena += "·Prob. LLuvia: " + (long)(media_prob_precipitacion_pasado > 0 ? media_prob_precipitacion_pasado + 0.01 : media_prob_precipitacion_pasado - 0.01)
+                        + "% " +"\n";
+
+                //ASIGNAMOS LA TEMPERATURA MAXIMA Y MINIMA AL TEXTVIEW.
+                cadena += "·Temperatura:" + "\n" +
+                        "-Máx: " +
+                        listaTempMax.item(6).getTextContent() +"ºC" + "\n"+
+                        "-Mín: "   +
+                        listaTempMin.item(6).getTextContent() +"ºC" + "\n" ;
+
+                cadena += "·Vientos:" + "\n" +
+                        "-Dirección:  " +
+                        listaDireccViento.item(14).getTextContent() + "\n"+
+                        "-Velocidad:  " +
+                        listaVientoVelocidad.item(14).getTextContent()+" km/h" + "\n" ;
+
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+            return cadena;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+
+
+            textView_PasadoMañana.setText(result);
 
         }
 
